276dd24c1beca374e8a440504600ff7f53a686e1
Hugo Nuñez Romante <hnromante@MacBook-Air-de-Hugo.local>
2018-04-13 10:51:53 -0300

    Creado modelo de tareas

--- src/main_app/models.py
+++ src/main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarea(models.Model):
+    nombre = models.CharField(max_length = 50)
+    descripcion = models.CharField(max_length = 50)
+    inicio = models.DateTimeField(auto_now_add=True, blank=True)
+    termino = models.DateTimeField(auto_now_add=False, blank=True)
+    TERMINADA = 'terminada'
+    PENDIENTE = 'pendiente'
+    CANCELADA = 'cancelada'
+    LIMITE = 'limite'
+    ESTADOS_TAREAS = (
+        (TERMINADA, 'Terminada'),
+        (PENDIENTE, 'Pendiente'),
+        (CANCELADA, 'Cancelada'),
+        (LIMITE, 'Limite'),
+    )
+    estado = models.CharField(
+        choices = ESTADOS_TAREAS,
+        default = PENDIENTE,
+        max_length = 30
+    )
+    img = models.CharField(max_length=255,blank=True)
--- src/main_app/views.py
+++ src/main_app/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html',{'tareas':tareas})
+    return render(request,'base_main_app.html')
+
+def tareas(request):
+    return render(request,'tareas.html',{'tareas':listado_tareas})
 
 class Tarea:
     def __init__(self,nombre,descripcion,inicio,termino,estado,img):
@@ -14,9 +17,10 @@ class Tarea:
         self.estado = estado
         self.img = img
 
-tareas = [
+listado_tareas = [
     Tarea('Aprender Django','los fundamentos entre el jeuves y el sabado','hoy','sabado','pendiente','https://i.imgflip.com/1freth.jpg'),
     Tarea('Leer Pressman','capitulo 1 al 6, metodologias ágiles, scrum y spike','viernes','domingo','pendiente','https://i.imgflip.com/1freth.jpg'),
     Tarea('Crear app de tareas','aplicacion para gestionar tareas del grupo y de paso practicar Django','hoy','lunes','pendiente','https://i.imgflip.com/1freth.jpg'),
 
 ]
+
